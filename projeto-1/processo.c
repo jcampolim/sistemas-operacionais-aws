b052200edbdd59a3dc63299d1b196235ac1489f1
Cloud User <ec2-user@ip-172-31-18-255.ec2.internal>
2024-04-13 22:37:00 +0000

    Adicionando arquivos de processos e threads

--- projeto-1/processo.c
+++ projeto-1/processo.c
@@ -1,167 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+int tempo = 0;
+ 
+// Todas as vezes que eh preciso atualizar o tempo, essa funcao chamada, ela eh responsavel por
+// criar um processo filho para atualizar o valor do tempo
+void processo(int t) {
+    // O ID do processo, necessario para diferenciar o processo filho do processo filho
+    pid_t pid;
+    // Vetor de duas posicoes que sera usado como file descriptor para especificar o pipe que esta sendo usado
+    int fd[2];
+
+    // Cria o pipe 
+    if(pipe(fd) < 0) {
+        exit(0);
+    }
+
+    // Cria o processo filho
+    pid = fork();
+    // Se ocorre algum erro durante a criacao do processo filho, o ID do processo eh menor que zero
+    if(pid < 0) {
+        fprintf(stderr, "fork failed");
+    // Se o ID do processo for 0, entao eh o processo filho que esta executando
+    } else if(pid == 0) {
+        // Fecha a entrada de leitura do pipe
+        close(fd[0]);
+        tempo = t;
+
+        // Escreve o valor atualizado do tempo no pipe
+        write(fd[1], &tempo, sizeof(tempo));
+        // Fecha a entrada de ecrita no pipe
+        close(fd[1]);
+
+        // O processo filho para de excutar
+        exit(0);
+    // Se o ID for maior do que zero, então eh o processo pai que esta sendo exacutado
+    } else {
+        // Fecha a entrada de escrita do pipe 
+        close(fd[1]);
+        // Espera o processo filho terminar de executar
+        wait(NULL);
+        // Le o valor atualizado do tempo do pipe
+        read(fd[0], &tempo, sizeof(tempo));
+
+        // Fecha a entrada de leitura do pipe
+        close(fd[0]);
+    }
+}
+
 int main() {
-    int n;
+    int n, direcao, direcaoInicial;
     scanf("%d", &n);
 
-    //Variáveis para armazenar a direção e os tempos de cada pessoa
-    int direcao, pessoas[2][n], tempo = 0;
+    int **pessoas = (int **)malloc(n * sizeof(int *));
+    pessoas[0] = (int *)malloc(n * sizeof(int));
+    pessoas[1] = (int *)malloc(n * sizeof(int));
 
-    //Armazenam os IDs dos processos e os descritores de arquivos dos pipes
-    pid_t pid[n+1];
-    int fd[n+1][2];
+    int tam[] = {0, 0};
+    while(tam[0] + tam[1] < n) {
+        int aux;
+        scanf("%d %d", &aux, &direcao);
 
-    //Cria os pipes para comunicação entre os processos
-    for(int i = 0; i <= n; i++) {
-      if(pipe(fd[i]) < 0) {
-        exit(0);
-      }
-    }
-
-    //Lê os tempos e as direções de cada pessoa
-    //Se for a primeira pessoa, define a direção
-    //Armazena o tempo e a direção da pessoa no array pessoas
-    int i = 0, j = 0;
-    while(i + j < n) {
-        int auxDir, aux;
-        scanf("%d %d", &aux, &auxDir);
-
-        if(i == 0) {
-            direcao = auxDir;
+        if(tam[0] + tam[1] == 0) {
+            direcaoInicial = direcao;
         }
-
-        if(auxDir == 0) {
-            pessoas[0][i] = aux; //Pessoas indo para a esquerda (0)
-            i++;
-        } else {
-            pessoas[1][j] = aux; //Pessoas indo para a direita (1)
-            j++;
-        }
-    }
-
-    //Armazena o número de pessoas em cada direção
-    int n0 = i, n1 = j;
-    i = 0; j = 0;
-
-    //Demonstra o movimento da primeira pessoa
-    pid[0] = fork();
-    if(pid[0] < 0) {
-        fprintf(stderr, "fork failed");
-    } else if(pid[0] == 0) {
-        close(fd[0][0]);
-        tempo += pessoas[direcao][0];
-        write(fd[0][1], &tempo, sizeof(tempo));
-        close(fd[0][1]);
-
-        return 0;
-    } else {
-        close(fd[0][1]);
-        wait(NULL);
-        read(fd[0][0], &tempo, sizeof(tempo));
-        close(fd[0][0]);
-    }
-
-    while(i + j < n) {
+    
         if(direcao == 0) {
-            pid[i+j+1] = fork();
-            if(pid[i+j+1] < 0) {
-                fprintf(stderr, "fork failed");
-            } else if(pid[i+j+1] == 0) {
-                //Código executado pelo processo filho
-                //Adiciona o tempo da primeira pessoa ao tempo total
-                // Escreve o tempo no pipe
-                close(fd[i+j+1][0]);
-                tempo += 10;
-                write(fd[i+j+1][1], &tempo, sizeof(tempo));
-                close(fd[i+j+1][1]);
-
-                return 0;
-            } else {
-                //Código executado pelo processo pai
-                //Espera o processo filho terminar
-                //Lê o tempo do pipe
-                close(fd[i+j+1][1]);
-                wait(NULL);
-                read(fd[i+j+1][0], &tempo, sizeof(tempo));
-                close(fd[i+j+1][0]);
-            }
-
-            i++;
-            //Simula o movimento das outras pessoas
-            //Se a direção atual é para a esquerda, cria um processo filho para simular a próxima pessoa
-            while(pessoas[0][i] < tempo && i < n0) {
-                pid[i+j+1] = fork();
-                if(pid[i+j+1] < 0) {
-                    fprintf(stderr, "fork failed");
-                } else if(pid[i+j+1] == 0) {
-                    close(fd[i+j+1][0]);
-                    tempo += (pessoas[0][i] - pessoas[0][i-1]);
-                    write(fd[i+j+1][1], &tempo, sizeof(tempo));
-                    close(fd[i+j+1][1]);
-
-                    return 0;
-                } else {
-                    close(fd[i+j+1][1]);
-                    wait(NULL);
-                    read(fd[i+j+1][0], &tempo, sizeof(tempo));
-                    close(fd[i+j+1][0]);
-                }       
-                i++;
-            }
-            //Muda a direção da escada
-            direcao = 1;
+            pessoas[0][tam[0]] = aux;
+            tam[0]++;
         } else {
-            pid[i+j+1] = fork();
-            if(pid[i+j+1] < 0) {
-                fprintf(stderr, "fork failed");
-            } else if(pid[i+j+1] == 0) {
-                close(fd[i+j+1][0]);
-                // Adiciona um tempo fixo ao tempo total
-                tempo += 10;
-                // Escreve o tempo no pipe
-                write(fd[i+j+1][1], &tempo, sizeof(tempo));
-                close(fd[i+j+1][1]);
+            pessoas[1][tam[1]] = aux;
+            tam[1]++;
+        }
+    }
 
-                return 0;
-            } else {
-                close(fd[i+j+1][1]);
-                wait(NULL);
-                read(fd[i+j+1][0], &tempo, sizeof(tempo));
-                close(fd[i+j+1][0]);
+    int pos[] = {0, 0};
+    processo(pessoas[direcaoInicial][0]);
+    direcao = direcaoInicial;
+
+    while(pos[0] < tam[0] || pos[1] < tam[1]) {
+        if((pessoas[0][pos[0]] < pessoas[1][pos[1]] && pos[0] < tam[0]) || tam[1] == 0 || tam[1] == pos[1]) {
+            if(pessoas[0][pos[0]] >= tempo) {
+                processo(pessoas[0][pos[0]] + 10);
+                pos[0]++;
+                
+                direcao = 0;
             }
 
-            j++;
+            if(pos[0] + pos[1] == 0) {
+                processo(tempo + 10);
+                pos[0]++;
+            }
 
-            while(pessoas[1][j] < tempo && j < n1) {
-                pid[i+j+1] = fork();
-                if(pid[i+j+1] < 0) {
-                    fprintf(stderr, "fork failed");
-                } else if(pid[i+j+1] == 0) {
-                    close(fd[i+j+1][0]);
-                    tempo += (pessoas[1][j] - pessoas[1][j-1]);
-                    write(fd[i+j+1][1], &tempo, sizeof(tempo));
-                    close(fd[i+j+1][1]);
-
-                    return 0;
-                } else {
-                    close(fd[i+j+1][1]);
-                    wait(NULL);
-                    read(fd[i+j+1][0], &tempo, sizeof(tempo));
-                    close(fd[i+j+1][0]);
+            if(pessoas[0][pos[0]] <= tempo && pos[0] < tam[0]) {
+                if(direcao == 1) {
+                    while(pessoas[0][pos[0]] <= tempo && pos[0] < tam[0]) {
+                        pos[0]++;
+                    }
+                    pessoas[0][pos[0] - 1] = tempo;
+                    processo(tempo + 10);
+                }
+                while(pessoas[0][pos[0]] <= tempo && pos[0] < tam[0]) {
+                    processo(tempo + (pessoas[0][pos[0]] - pessoas[0][pos[0] - 1]));
+                    pos[0]++;
                 }
 
-                j++;
+                direcao = 0;
             }
-            //Muda a direção da escada
-            direcao = 0;
+        } else if((pessoas[0][pos[0]] > pessoas[1][pos[1]] && pos[1] < tam[1]) || tam[0] == 0 || tam[0] == pos[0]) {
+            if(pessoas[1][pos[1]] > tempo) {
+                processo(pessoas[1][pos[1]] + 10);
+                pos[1]++;
+                
+                direcao = 1;
+            }
+
+            if(pos[0] + pos[1] == 0) {
+                processo(tempo + 10);
+                pos[1]++;
+            }
+
+            if(pessoas[1][pos[1]] <= tempo && pos[1] < tam[1]) {
+                if(direcao == 0) {
+                    while(pessoas[1][pos[1]] <= tempo && pos[1] < tam[1]) {
+                        pos[1]++;
+                    }
+                    pessoas[1][pos[1] - 1] = tempo;
+                    processo(tempo + 10);
+                }
+                while(pessoas[1][pos[1]] <= tempo && pos[1] < tam[1]) {
+                    processo(tempo + (pessoas[1][pos[1]] - pessoas[1][pos[1] - 1]));
+                    pos[1]++;
+                }
+
+                direcao = 1;
+            }
         }
     }
-    // Exibe na tela a quantidade de tempo que a escada ficou ligada
+
     printf("%d\n", tempo);
+    free(pessoas);
 
     return 0;
 }
--- projeto-1/thread.c
+++ projeto-1/thread.c
@@ -2,105 +2,170 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-// Variável global com o momento que a última pessoa saiu da escada
 int tempo = 0;
 
-// Função que será executada quando a thread for criada e irá atualizar o valor do tempo
+// Funcao que sera executada quando a thread for criada e ira atualizar o valor do tempo
 void *attTempo(void *t) {
 	int *aux = (int *)t;
-	tempo += *aux;
+	tempo = *aux;
 }
 
 int main() {
-    int n;
+    // O ID da thread, necessario para identificar as threads
+    pthread_t tid; 
+
+    int n, direcao, direcaoInicial;
     scanf("%d", &n);
 
-    // Matriz com duas linhas, onde cada linha representa um sentido da escada rolante
-    int direcao, pessoas[2][n];
+    int **pessoas = (int **)malloc(n * sizeof(int *));
+    pessoas[0] = (int *)malloc(n * sizeof(int));
+    pessoas[1] = (int *)malloc(n * sizeof(int));
 
-	pthread_t tid;
+    int tam[] = {0, 0};
+    while(tam[0] + tam[1] < n) {
+        int aux;
+        scanf("%d %d", &aux, &direcao);
 
-    // Lê os valores de entrada e armazena na matriz
-    int i = 0, j = 0;
-    while(i + j < n) {
-        int auxDir, aux;
-        scanf("%d %d", &aux, &auxDir);
-
-        if(i == 0) {
-            direcao = auxDir;
+        if(tam[0] + tam[1] == 0) {
+            direcaoInicial = direcao;
         }
-
-        if(auxDir == 0) {
-            pessoas[0][i] = aux;
-            i++;
+    
+        if(direcao == 0) {
+            pessoas[0][tam[0]] = aux;
+            tam[0]++;
         } else {
-            pessoas[1][j] = aux;
-            j++;
+            pessoas[1][tam[1]] = aux;
+            tam[1]++;
         }
     }
 
-    // Define n0 como a quantidade de pessoas no sentido 0 e n1 como a quantidade 
-    // de pessoas no sentido 1
-    // A variável t armazena a quantidade de tempo que deve ser adicionada
-    int n0 = i, n1 = j, t = pessoas[direcao][0];
-    i = 0; j = 0;
+    int pos[] = {0, 0};
+    int t = pessoas[direcaoInicial][0];
 
-    // Comandos para criar e executar uma thread (a variável t é passada como parâmetro)
-    pthread_create(&tid, NULL, attTempo, (void*)&t);
+    // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+    // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+    pthread_create(&tid, NULL, attTempo, (void *)&t);
+    // Aguarda o termino da thread para continuar a execucao
     pthread_join(tid, NULL);
 
-    // Loop que percorre todos os valores da matriz
-    while(i + j < n) {
-        if(direcao == 0) {
-	        t = 10;
+    direcao = direcaoInicial;
 
-            // Adiciona 10 se entrar uma pessoa no sentido 0
-            pthread_create(&tid, NULL, attTempo, (void *)&t);
-	        pthread_join(tid, NULL);
+    while(pos[0] < tam[0] || pos[1] < tam[1]) {
+        if((pessoas[0][pos[0]] < pessoas[1][pos[1]] && pos[0] < tam[0]) || tam[1] == 0 || tam[1] == pos[1]) {
+            if(pessoas[0][pos[0]] >= tempo) {
+                t = pessoas[0][pos[0]] + 10;
 
-            i++;
-
-            // Adiciona o tempo correspondente das próximas pessoas que entram no sentido 0
-            // antes da escada rolante trocar de sentido
-            while(pessoas[0][i] < tempo && i < n0) {
-                t = (pessoas[0][i] - pessoas[0][i-1]);
-
+                // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
                 pthread_create(&tid, NULL, attTempo, (void *)&t);
+                // Aguarda o termino da thread para continuar a execucao
                 pthread_join(tid, NULL);
 
-                i++;
-            } 
+                pos[0]++;
+                
+                direcao = 0;
+            }
 
-            // Muda a direção da escada
-            direcao = 1;
-        } else {
-            t = 10;
+            if(pos[0] + pos[1] == 0) {
+                t  = tempo + 10;
 
-            // Adiciona 10 se entrar uma pessoa no sentido 1
-	        pthread_create(&tid, NULL, attTempo, (void *)&t);
-	        pthread_join(tid, NULL);
-
-            j++;
-
-            // Adiciona o tempo correspondente das próximas pessoas que entram no sentido 1
-            // antes da escada rolante trocar de sentido
-            while(pessoas[1][j] < tempo && j < n1) {
-                t = (pessoas[1][j] - pessoas[1][j-1]);
-
+                // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
                 pthread_create(&tid, NULL, attTempo, (void *)&t);
+                // Aguarda o termino da thread para continuar a execucao
                 pthread_join(tid, NULL);
 
-                j++;
-            } 
+                pos[0]++;
+            }
 
-            // Muda a direção da escada
-            direcao = 0;
+            if(pessoas[0][pos[0]] <= tempo && pos[0] < tam[0]) {
+                if(direcao == 1) {
+                    while(pessoas[0][pos[0]] <= tempo && pos[0] < tam[0]) {
+                        pos[0]++;
+                    }
+                    pessoas[0][pos[0] - 1] = tempo;
+                    t = tempo + 10;
+
+                    // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                    // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+                    pthread_create(&tid, NULL, attTempo, (void *)&t);
+                    // Aguarda o termino da thread para continuar a execucao
+                    pthread_join(tid, NULL);
+                }
+                while(pessoas[0][pos[0]] <= tempo && pos[0] < tam[0]) {
+                    t = tempo + (pessoas[0][pos[0]] - pessoas[0][pos[0] - 1]);
+
+                    // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                    // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+                    pthread_create(&tid, NULL, attTempo, (void *)&t);
+                    // Aguarda o termino da thread para continuar a execucao
+                    pthread_join(tid, NULL);
+
+                    pos[0]++;
+                }
+
+                direcao = 0;
+            }
+        } else if((pessoas[0][pos[0]] > pessoas[1][pos[1]] && pos[1] < tam[1]) || tam[0] == 0 || tam[0] == pos[0]) {
+            if(pessoas[1][pos[1]] > tempo) {
+                t = pessoas[1][pos[1]] + 10;
+
+                // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+                pthread_create(&tid, NULL, attTempo, (void *)&t);
+                // Aguarda o termino da thread para continuar a execucao
+                pthread_join(tid, NULL);
+
+                pos[1]++;
+                
+                direcao = 1;
+            }
+
+            if(pos[0] + pos[1] == 0) {
+                t = tempo + 10;
+
+                // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+                pthread_create(&tid, NULL, attTempo, (void *)&t);
+                // Aguarda o termino da thread para continuar a execucao
+                pthread_join(tid, NULL);
+
+                pos[1]++;
+            }
+
+            if(pessoas[1][pos[1]] <= tempo && pos[1] < tam[1]) {
+                if(direcao == 0) {
+                    while(pessoas[1][pos[1]] <= tempo && pos[1] < tam[1]) {
+                        pos[1]++;
+                    }
+                    pessoas[1][pos[1] - 1] = tempo;
+                    t = tempo + 10;
+
+                    // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                    // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+                    pthread_create(&tid, NULL, attTempo, (void *)&t);
+                    // Aguarda o termino da thread para continuar a execucao
+                    pthread_join(tid, NULL);
+                }
+                while(pessoas[1][pos[1]] <= tempo && pos[1] < tam[1]) {
+                    t = tempo + (pessoas[1][pos[1]] - pessoas[1][pos[1] - 1]);
+
+                    // Funcao para criar uma thread, o primeiro parametro eh o ID da thread, o segundo sao os atributos (podem ser NULL),
+                    // o terceiro eh o endereco da funcao que sera executada pela thread e o ultimo sao os parametros da funcao
+                    pthread_create(&tid, NULL, attTempo, (void *)&t);
+                    // Aguarda o termino da thread para continuar a execucao
+                    pthread_join(tid, NULL);
+
+                    pos[1]++;
+                }
+
+                direcao = 1;
+            }
         }
     }
 
-    // Exibe na tela a quantidade de tempo que a escada ficou ligada
     printf("%d\n", tempo);
-    exit(0);
+    free(pessoas);
 
     return 0;
 }
